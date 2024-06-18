4b52de587003a42f218ba440551ef5a961dfeccf
broGideon <turuncev70@gmail.com>
2024-06-18 03:16:42 +0300

    fix switch theme

--- FinalLab/ViewModel/Windows/PatientViewModel.cs
+++ FinalLab/ViewModel/Windows/PatientViewModel.cs
@@ -8,6 +8,8 @@ namespace FinalLab.ViewModel.Windows;
 
 public class PatientViewModel : BindingHelper
 {
+    public List<string> Themes { get; set; } = new List<string> { "Светлая", "Темная"};
+    
     private List<Patient> _patients = null!;
 
     public event EventHandler SwitchUsers;
@@ -25,16 +27,36 @@ public class PatientViewModel : BindingHelper
         get => _currentPatient;
         set => SetField(ref _currentPatient, value);
     }
+
+    private string _currentTheme;
+
+    public string CurrentTheme
+    {
+        get => _currentTheme;
+        set => SetField(ref _currentTheme, value);
+    }
     
     public PatientViewModel()
     {
         Patients = JsonConvert.DeserializeObject<List<Patient>>(Settings.Default.CurrentUsers)!;
         CurrentPatient = Patients[0];
+        if (App.Theme == "Light")
+            CurrentTheme = "Светлая";
+        else
+            CurrentTheme = "Темная";
     }
 
     public void SelectionPatient(object sender, SelectionChangedEventArgs e)
     {
         CurrentPatient = (((sender as ComboBox)!).SelectedItem as Patient)!;
         SwitchUsers(this, EventArgs.Empty);
+    }
+    
+    public void SelectionTheme(object sender, SelectionChangedEventArgs e)
+    {
+        if (CurrentTheme == "Светлая")
+            App.Theme = "Light";
+        else
+            App.Theme = "Dark";
     }
 }
